0ac8a076fc1a421ec3e80b4244b7a10e17695e6b
Alisa Leung <alisal.singyee@gmail.com>
2025-10-28 17:40:08 -0700

    maybe works idk

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
     //constants
     const fileInput = document.getElementById("photoFile");
     const imagePreview = document.getElementById("imagePreview");
+    const imageContainer = document.getElementById("galleryContainer");
     //running functions
     timeGreeting();
+    loadGallery();
 
-    loadPhoto;
     fileInput.addEventListener("change", (event) => {
         const file = event.target.files[0];
         if (file) {
@@ -26,24 +27,30 @@ function timeGreeting(){
     const hour = new Date().getHours();
     const greetingElement = document.getElementById("greeting");
     if (hour < 12){
-        greetingElement.innerHTML = "good morning, bloomer.";
+        greetingElement.innerHTML = "Good morning, bloomer.";
     } else if (hour < 18){
-        greetingElement.innerHTML = "good afternoon, bloomer.";
+        greetingElement.innerHTML = "Good afternoon, bloomer.";
     } else{
-        greetingElement.innerHTML = "good evening, bloomer.";
+        greetingElement.innerHTML = "Good evening, bloomer.";
     }
 }
-//saves photo to chrome's storage
+//saves photo to chrome's local storage
 function savePhoto(imageUrl){
     chrome.storage.local.set({uploadedPhoto: imageUrl}, () => {
         console.log("photo saved to storage");
     });
 }
-//loads previously saved images from chrome's storage
-function loadPhoto(){
-    chrome.storage.local.get("uploadedPhoto", (data) => {
-        if (data.uploadedPhoto){
-            imagePreview.src = data.uploadedPhoto;
-        }
+//loads previously saved images from chrome's storage and gallery when extension is opened
+function loadGallery(){
+    chrome.storage.local.get(['gallery'], (result) => {
+        chrome.storage.local.get("uploadedPhoto", (data) => {
+            if (data.uploadedPhoto){
+                const img = document.createElement('img');
+                img.src = data.uploadedPhoto;
+                img.className = 'galleryImage';
+                container.appendChild(img);
+                imagePreview.src = data.uploadedPhoto;
+            }
+        });
     });
 }
