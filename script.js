729af4b1bf023757290d68912b7cd5a433d1278e
Alisa Leung <alisal.singyee@gmail.com>
2025-10-29 16:12:22 -0700

    ok redoing upload system

--- script.js
+++ script.js
@@ -2,23 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
     //constants
     const fileInput = document.getElementById("photoFile");
     const imagePreview = document.getElementById("imagePreview");
-    const imageContainer = document.getElementById("galleryContainer");
+    const imageGallery = document.getElementsByClassName("galleryImage");
     //running functions
     timeGreeting();
-    loadGallery();
 
-    fileInput.addEventListener("change", (event) => {
-        const file = event.target.files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                const imageUrl = e.target.result;
-                imagePreview.src = imageUrl;
-                savePhoto(imageUrl);
-            };
-            reader.readAsDataURL(file);
-        }
-    });
 });
 
 //functions
@@ -33,24 +20,4 @@ function timeGreeting(){
     } else{
         greetingElement.innerHTML = "Good evening, bloomer.";
     }
-}
-//saves photo to chrome's local storage
-function savePhoto(imageUrl){
-    chrome.storage.local.set({uploadedPhoto: imageUrl}, () => {
-        console.log("photo saved to storage");
-    });
-}
-//loads previously saved images from chrome's storage and gallery when extension is opened
-function loadGallery(){
-    chrome.storage.local.get(['gallery'], (result) => {
-        chrome.storage.local.get("uploadedPhoto", (data) => {
-            if (data.uploadedPhoto){
-                const img = document.createElement('img');
-                img.src = data.uploadedPhoto;
-                img.className = 'galleryImage';
-                container.appendChild(img);
-                imagePreview.src = data.uploadedPhoto;
-            }
-        });
-    });
 }
